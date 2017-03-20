94b80163bd6575403d750cfb70b3a09ad60e0e74
Calvin <calvinlobo@gmail.com>
2017-03-19 21:50:20 -0400

    Added a Label class which inheirits from Widget

--- src/loboutils.cpp
+++ src/loboutils.cpp
@@ -10,34 +10,45 @@ char const* help()
 
 class Widget {
 
-    std::string msg;
-
 	public: 
 		int x, y, width, height;
-		void move(int, int);
-		int right(void);
-		Widget(int, int, int, int);
+
+		// instance method 
+		void move(int dx, int dy) { 
+			this->x += dx;
+			this->y += dy;
+		}
+
+		// getter function accessed via python through @property
+		int right(void) {
+			return x + width;
+		}
+
+		// constructor function (equivalent to __init__ for python classes)
+		Widget(int x, int y, int width, int height) {
+			this->x = x;
+			this->y = y;
+			this->width = width;
+			this->height = height;
+		}
 
 };
 
 
-Widget::Widget(int x, int y, int width, int height) {
-	this->x = x;
-	this->y = y;
-	this->width = width;
-	this->height = height;
-}
 
-// instance method 
-void Widget::move(int dx, int dy) { 
-	this->x += dx;
-	this->y += dy;
-}
+// Create a Label class that inheirits from Widget
+class Label : public Widget {
+	public:
+		// In addition to widget's public attributes, define a name for the label
+		std::string name;
+		// Create a constructor for the Label and inside create an instance of the base class (Widget). 
+ 		// Creating the base class instance will initialize the base class variables and executes it's constructor. 
+		// In addition, we've added an argument for the 'name' attribute
+		Label(std::string name, int x, int y, int width, int height): Widget(x, y, width, height) {
+			this->name = name;		
+		};
 
-// getter function
-int Widget::right(void) {
-	return x + width;
-}
+};
 
 
 
@@ -47,13 +58,24 @@ BOOST_PYTHON_MODULE(loboutils)
     def("help", help);
 
 	
+	// Expose the class by wrapping it with the class_ template. The first argument is the name to expose it as and the second
+	// argument is the constructor function
     class_<Widget>("Widget", init<int, int, int, int>())
 		
         .def("move", &Widget::move)			// expose the defined a class method
 		.def_readwrite("x", &Widget::x)		// expose the class attribute as read-write, other option is def_readonly
 		.def_readwrite("y", &Widget::y)
-		.add_property("right", &Widget::right)
+		.def_readwrite("width", &Widget::width)	
+		.def_readwrite("height", &Widget::height)
+
+		.add_property("right", &Widget::right) // expose the function as a @property 
     ;
+
+	// Expose the Label class which inheirits from Widget. We do not need to expose functions and variables
+	// that have already been exposed in the base class.
+    class_<Label, bases<Widget> >("Label", init<std::string, int, int, int, int>())
+		.def_readwrite("name", &Label::name)
+	;		
 
 }
 
--- test.py
+++ test.py
@@ -5,10 +5,11 @@ print L.help()
 
 w = L.Widget(0, 0, 10, 20)
 
-
-
 print w.x
 w.move(10, 5)
-print w.x, w.y
-
+print w.x, w.y, w.width, w.height
 print 'right', w.right
+
+
+l = L.Label('Label A', 1, 2, 5, 10)
+print l.name, l.x, l.y, l.width, l.height
